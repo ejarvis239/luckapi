91c602871d97ef48e6d5ed6922a4ca2625a036f7
Emma Jarvis <Emma.Jarvis@inenco.com>
2020-07-05 14:43:45 +0100

    server

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require ('cors');
+const helmet = require ('helmet');
+const bodyParser = require('body-parser');
+const app = express();
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+// mount all routes on /api path
+app.use('/api', routes);
+
+export default app;
